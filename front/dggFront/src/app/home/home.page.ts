fec3c472d5de5f38d2a0155aa6ab959d6ad3178b
Marcos Palmeira <kinglordbr@gmail.com>
2025-09-23 19:18:39 -0300

    ajeitei ali

--- front/dggFront/src/app/home/home.page.ts
+++ front/dggFront/src/app/home/home.page.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { IonRouterLinkWithHref } from "../../../node_modules/@ionic/angular/standalone/navigation/router-link-delegate";
-import { RouterLink } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  startGame() {
+    console.log("Novo jogo iniciado!");
+    // exemplo: navegar para a tela do jogo
+    this.router.navigate(['/game']);
+  }
+
+  openSaves() {
+    console.log("Abrindo saves...");
+    this.router.navigate(['/saves']);
+  }
+
+  showsettings() {
+    console.log("Abrindo configurações...");
+    this.router.navigate(['/settings']);
+  }
+
+  exitGame() {
+    console.log("Saindo do jogo...");
+    // no navegador não dá pra fechar janela direto, mas pode redirecionar
+    this.router.navigate(['/exit']);
+  }
 }
